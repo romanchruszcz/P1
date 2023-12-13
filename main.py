2a3095070e67701d306c21fda14cc5544ed242d6
roman_chruszcz <r.chruszcz@hotmail.com>
2023-12-13 14:34:08 +0100

    13.12

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk 
 
+# settings for fonts and colors for backgrounds
 general_bg_color = "#cdced1"
 frame_color = "black"
 active_color = "white"
@@ -17,7 +18,7 @@ root.geometry("1920x1080")
 root.config(background=general_bg_color)
 frame_Top = Frame(root,width=1500,height=115,background=general_bg_color,highlightthickness=1,highlightbackground=frame_color,highlightcolor=frame_color)
 frame_Top.place(x=5,y=10)
-label_in_Top = Label(frame_Top, width = 77, height= 1, background=active_color,borderwidth=2,relief="solid",text="DANE PODSTAWOWE")
+label_in_Top = Label(frame_Top, width = 77, height= 1, background=active_color,borderwidth=2,relief="solid",text="DANE PODSTAWOWE")  # basic informations
 label_in_Top.configure(font=font_big)
 label_in_Top.place(x=10,y=10)
 date_label_A = Label(frame_Top,text="Data:",background=general_bg_color)
@@ -37,7 +38,7 @@ shift_list.place(x=360,y=70)
 work_time_label = Label(frame_Top,text="Czas pracy:",background=general_bg_color)
 work_time_label.configure(font=font_mid)
 work_time_label.place(x=490,y=70)
-work_time_list = ttk.Combobox(frame_Top,values=[480,600,60,120,180,340,400,540],width=5)
+work_time_list = ttk.Combobox(frame_Top,values=[480,600,60,120,180,340,400,540],width=5)     # how long time production will work  choice done by user
 work_time_list.current(0)
 work_time_list.configure(font=font_mid)
 work_time_list.place(x=650,y=70)
@@ -90,7 +91,7 @@ current_order_treeview.column('Nr zlecenia',minwidth=0,width=100,stretch=NO,anch
 current_order_treeview.heading('Nr produktu', text='Nr produktu')
 current_order_treeview.column('Nr produktu', minwidth=0,width=220,stretch=NO,anchor=CENTER)
 
-current_order_treeview.heading('Przezbrojenie rozp.', text='Przezbrojenie rozp.')
+current_order_treeview.heading('Przezbrojenie rozp.', text='Przezbrojenie rozp.') 
 current_order_treeview.column('Przezbrojenie rozp.',  minwidth=0,width=112,stretch=NO,anchor=CENTER)
 
 
@@ -129,7 +130,101 @@ order_start_button.place(x=10,y=300)
 order_start_button = Button(root,width=25,text="Zakończenie zlecenia",font=font_small,background=ending_color)
 order_start_button.place(x=1190,y=300) 
 
+frame_Bottom = Frame(root,width=1500,height=570,background=general_bg_color,highlightthickness=1,highlightbackground=frame_color,highlightcolor=frame_color)
+frame_Bottom.place(x=5,y=355)
+
+label_in_Bottom = Label(frame_Bottom, width = 77, height= 1, background=active_color,borderwidth=2,relief="solid",text="LISTA ZLECEŃ ZAKOŃCZONYCH",foreground="BLUE")
+label_in_Bottom.configure(font=font_big)
+label_in_Bottom.place(x=10,y=10)
+
+current_order_columns = ('Nr zlecenia',
+                         'Nr produktu',
+                         'Przezbrojenie rozp.',
+                         'Typ przezb.',
+                         'Przezbrojenie zakoń.',
+                         'Suma strat',
+                         'Ilość wyprodukowana',
+                         'Godz. rozpoczęcia',
+                         'Godz. zakończenia',
+                         'Czas pracy',
+                         'Cel',
+                         'Wynik OEE')
+finished_order_treeview = ttk.Treeview(frame_Bottom,columns=current_order_columns,show="headings",height=22)
+finished_order_treeview.heading('Nr zlecenia', text='Nr zlecenia')
+finished_order_treeview.column('Nr zlecenia',minwidth=0,width=100,stretch=NO,anchor=CENTER)
+
+finished_order_treeview.heading('Nr produktu', text='Nr produktu')
+finished_order_treeview.column('Nr produktu', minwidth=0,width=220,stretch=NO,anchor=CENTER)
+
+finished_order_treeview.heading('Przezbrojenie rozp.', text='Przezbrojenie rozp.') 
+finished_order_treeview.column('Przezbrojenie rozp.',  minwidth=0,width=112,stretch=NO,anchor=CENTER)
+
+
+finished_order_treeview.heading('Typ przezb.', text='Typ przezb.')
+finished_order_treeview.column('Typ przezb.', minwidth=0,width=90,stretch=NO,anchor=CENTER)
+
+finished_order_treeview.heading('Przezbrojenie zakoń.', text='Przezbrojenie zakoń.')
+finished_order_treeview.column('Przezbrojenie zakoń.', minwidth=0,width=114,stretch=NO,anchor=CENTER)
+
+
+finished_order_treeview.heading('Suma strat', text='Suma strat')
+finished_order_treeview.column('Suma strat', minwidth=0,width=100,stretch=NO,anchor=CENTER)
+
+finished_order_treeview.heading('Ilość wyprodukowana', text='Ilość wyprodukowana')
+finished_order_treeview.column('Ilość wyprodukowana', minwidth=0,width=130,stretch=NO,anchor=CENTER)
+
+finished_order_treeview.heading('Godz. rozpoczęcia', text='Godz. rozpoczęcia')
+finished_order_treeview.column('Godz. rozpoczęcia', minwidth=0,width=130,stretch=NO,anchor=CENTER)
+
+
+finished_order_treeview.heading('Godz. zakończenia', text='Godz. zakończenia')
+finished_order_treeview.column('Godz. zakończenia', minwidth=0,width=130,stretch=NO,anchor=CENTER)
+
+finished_order_treeview.heading('Czas pracy', text='Czas pracy')
+finished_order_treeview.column('Czas pracy', minwidth=0,width=100,stretch=NO,anchor=CENTER)
+
+finished_order_treeview.heading('Cel', text='Cel')
+finished_order_treeview.column('Cel', minwidth=0,width=120,stretch=NO,anchor=CENTER)
+
+finished_order_treeview.heading('Wynik OEE', text='Wynik OEE')
+finished_order_treeview.column('Wynik OEE',minwidth=0,width=120,stretch=NO,anchor=CENTER)
+finished_order_treeview.place(x=10,y=70)
+
+
+
 
 #mylist=(434343,3434343434,22,2255,2424,2424)
 #current_order_treeview.insert('',END,values=mylist)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 root.mainloop()
