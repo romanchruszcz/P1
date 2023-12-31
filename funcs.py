549a719cec8b066b01c5fd8c57593579115d447d
Roman Chruszcz <r.chruszcz@hotmail.com>
2023-12-31 16:19:28 +0100

    31.12.1

--- funcs.py
+++ funcs.py
@@ -3,3 +3,5 @@ import datetime
 
 date_now = datetime.datetime.now()
 date_converted = date_now.strftime("%Y-%m-%d")
+time_converted = date_now.strftime("%H:%M")
+print(time_converted)
--- order.py
+++ order.py
@@ -28,13 +28,23 @@ def orders_window():
     order_window.title("Rozpoczęcie / Zakończenie zlecenia")
     order_window.configure(background=general_bg_color)
 
-    general_frame = Frame(order_window,width=1190,height=790,background=general_bg_color,highlightbackground=frame_color,highlightcolor=frame_color,highlightthickness=1)
+    general_frame = Frame(order_window,width=1190,height=300,background=general_bg_color,highlightbackground=frame_color,highlightcolor=frame_color,highlightthickness=1)
     general_frame.place(x=5,y=5)
+
+    label_in_Top = Label(general_frame, width = 61, height= 1, background=active_color,borderwidth=2,relief="solid",text="Rozpoczęcie zlecenia")  # basic informations
+    label_in_Top.configure(font=font_big)
+    label_in_Top.place(x=10,y=10)
+
     date_label = Label(general_frame,text="Data:", font=font_small,background=general_bg_color)
-    date_label.place(x=5,y=5)
+    date_label.place(x=10,y=65)
     date_entry = Entry(general_frame,width=10,font=font_small,highlightbackground=frame_color,highlightcolor=frame_color,highlightthickness=1,justify=RIGHT)
-    date_entry.place(x=70,y=5)
+    date_entry.place(x=80,y=65)
     date_entry.insert(0,funcs.date_converted)
 
+    time_label = Label(general_frame,text="Godzina startu:", font=font_small,background=general_bg_color)
+    time_label.place(x=210,y=65)
+    time_entry = Entry(general_frame,width=5,font=font_small,highlightbackground=frame_color,highlightcolor=frame_color,highlightthickness=1,justify=RIGHT)
+    time_entry.place(x=370,y=65)
+    time_entry.insert(0,funcs.time_converted)
 
     order_window.mainloop()
